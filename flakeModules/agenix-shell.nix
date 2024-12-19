a9e73b37266d63fb5b94cd27bf729885e3df3d19
Matt Schreiber <tomeon@dogea.red>
2024-12-19 16:13:55 -0500

    fix(module): namespace vars with `__agenix_shell_`
    to avoid potential conflicts with existing variables in the user's shell
    environment.
    
    Additionally, unset all variables beginning with `__agenix_shell_` at
    the end of the hook script.

--- flakeModules/agenix-shell.nix
+++ flakeModules/agenix-shell.nix
@@ -101,40 +101,45 @@ in {
             # shellcheck disable=SC2086
             rm -rf "${cfg.secretsPath}"
 
-            IDENTITIES=()
+            __agenix_shell_identities=()
             # shellcheck disable=2043
-            for identity in ${builtins.toString cfg.identityPaths}; do
-              test -r "$identity" || continue
-              IDENTITIES+=(-i)
-              IDENTITIES+=("$identity")
+            for __agenix_shell_identity in ${builtins.toString cfg.identityPaths}; do
+              test -r "$__agenix_shell_identity" || continue
+              __agenix_shell_identities+=(-i "$__agenix_shell_identity")
             done
 
-            test "''${#IDENTITIES[@]}" -eq 0 && echo "[agenix] WARNING: no readable identities found!"
+            test "''${#__agenix_shell_identities[@]}" -eq 0 && echo "[agenix] WARNING: no readable identities found!"
 
             mkdir -p "${cfg.secretsPath}"
           ''
-          + lib.concatStrings (lib.mapAttrsToList (_: config.agenix-shell._installSecret) cfg.secrets);
+          + lib.concatStrings (lib.mapAttrsToList (_: config.agenix-shell._installSecret) cfg.secrets)
+          + ''
+            # Clean up after ourselves
+            # shellcheck disable=SC2154
+            unset "''${!__agenix_shell_@}" || :
+          '';
       };
 
       _installSecret = mkOption {
         type = types.functionTo types.str;
         internal = true;
         default = secret: ''
-          SECRET_PATH=${secret.path}
+          __agenix_shell_secret_path=${secret.path}
 
           # shellcheck disable=SC2193
-          [ "$SECRET_PATH" != "${cfg.secretsPath}/${secret.name}" ] && mkdir -p "$(dirname "$SECRET_PATH")"
+          [ "$__agenix_shell_secret_path" != "${cfg.secretsPath}/${secret.name}" ] && mkdir -p "$(dirname "$__agenix_shell_secret_path")"
+
           (
             umask u=r,g=,o=
             test -f "${secret.file}" || echo '[agenix] WARNING: encrypted file ${secret.file} does not exist!'
-            test -d "$(dirname "$SECRET_PATH")" || echo "[agenix] WARNING: $(dirname "$SECRET_PATH") does not exist!"
-            LANG=${config.i18n.defaultLocale or "C"} ${lib.getExe config.agenix-shell.agePackage} --decrypt "''${IDENTITIES[@]}" -o "$SECRET_PATH" "${secret.file}"
+            test -d "$(dirname "$__agenix_shell_secret_path")" || echo "[agenix] WARNING: $(dirname "$__agenix_shell_secret_path") does not exist!"
+            LANG=${config.i18n.defaultLocale or "C"} ${lib.getExe config.agenix-shell.agePackage} --decrypt "''${__agenix_shell_identities[@]}" -o "$__agenix_shell_secret_path" "${secret.file}"
           )
 
-          chmod ${secret.mode} "$SECRET_PATH"
+          chmod ${secret.mode} "$__agenix_shell_secret_path"
 
-          ${secret.name}=$(cat "$SECRET_PATH")
-          ${secret.namePath}="$SECRET_PATH"
+          ${secret.name}=$(cat "$__agenix_shell_secret_path")
+          ${secret.namePath}="$__agenix_shell_secret_path"
           export ${secret.name}
           export ${secret.namePath}
         '';
