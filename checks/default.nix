444834c2374cdd765052c762fc3c4cda11819f8e
Matt Schreiber <tomeon@dogea.red>
2025-01-18 09:00:29 -0500

    refactor(checks): factor out check instantiation

--- checks/default.nix
+++ checks/default.nix
@@ -1,21 +1,43 @@
-flakePartsArgs @ {self, ...}: {
-  perSystem = {
-    pkgs,
-    config,
-    lib,
-    ...
-  }: {
+{
+  config,
+  inputs,
+  lib,
+  self,
+  ...
+}: {
+  perSystem = {pkgs, ...} @ perSystem: let
+    callFlake = src:
+      lib.fix (flake:
+        (import src).outputs (inputs
+          // {
+            self = flake // {inherit inputs;};
+            agenix-shell = config.flake;
+          }));
+
+    templateCheck = {
+      flakeSrc,
+      templateSrc,
+      ...
+    } @ args:
+      pkgs.callPackage ./template-check.nix (builtins.removeAttrs args ["flakeSrc" "templateSrc"]
+        // {
+          src = templateSrc;
+          flake = callFlake flakeSrc;
+        });
+  in {
     checks = {
-      flake-parts-template = pkgs.callPackage ./flake-parts-template.nix {
-        inherit flakePartsArgs;
+      flake-parts-template = templateCheck {
+        flakeSrc = "${config.flake.templates.flake-parts.path}/flake.nix";
+        templateSrc = ../templates/flake-parts;
       };
 
-      basic-template = pkgs.callPackage ./basic-template.nix {
-        inherit flakePartsArgs;
+      basic-template = templateCheck {
+        flakeSrc = "${config.flake.templates.basic.path}/flake.nix";
+        templateSrc = ../templates/basic;
       };
 
       formatting = pkgs.runCommand "check-formatting" {} ''
-        ${lib.getExe config.formatter} --check ${self.outPath} > $out
+        ${lib.getExe perSystem.config.formatter} --check ${self.outPath} > $out
       '';
     };
   };
